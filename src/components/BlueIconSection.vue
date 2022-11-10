d53a3586a5c6b8a7cf908ca254d2261a48c1e059
StefaniaSgreva <stefaniasgreva@gmail.com>
2022-11-10 21:39:35 +0100

    added blu component with icons styles

--- src/components/BlueIconSection.vue
+++ src/components/BlueIconSection.vue
@@ -1,7 +1,26 @@
 <template>
     <section class="menu-icons">
         <div class="wrapper">
-            Icons
+            <div class="digital-comics">
+                <img src="../assets/img/buy-comics-digital-comics.png" alt="Digital Comics">
+                <span>Digital Comics</span>
+            </div>
+            <div class="merciandise">
+                <img src="../assets/img/buy-comics-merchandise.png" alt="DC Merciandise">
+                <span>DC Merciandise</span>
+            </div>
+            <div class="subscriptions">
+                <img src="../assets/img/buy-comics-subscriptions.png" alt="Subscriptions">
+                <span>Subscriptions</span>
+            </div>
+            <div class="shop-locator">
+                <img src="../assets/img/buy-comics-shop-locator.png" alt="Shop Locator">
+                <span>Comic Shop Locator</span>
+            </div>
+            <div class="power-visa">
+                <img src="../assets/img/buy-dc-power-visa.svg" alt="DC Power Visa">
+                <span>DC Power Visa</span>
+            </div>
         </div>
     </section>
 </template>
@@ -20,5 +39,27 @@
         background-color: $blue;
         color: $white;
         @include dflex;
+
+        div{
+            display: flex;
+            justify-content: space-around;
+            align-items: center;
+        }
+        img{
+            width: 3.2rem;
+            margin-right: 1rem;
+        }
+        .digital-comics img{
+            width: 2.7rem;
+
+        }
+        .shop-locator img{
+            width: 2.1rem;
+
+        }
+        span{
+            text-transform: uppercase;
+            font-size: .75rem;
+        }
     }
 </style>
