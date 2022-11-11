b35528f68665d8f0d5d55fe43969d0a48d119319
StefaniaSgreva <stefaniasgreva@gmail.com>
2022-11-11 16:25:39 +0100

    added current series section

---
+++ src/components/CurrentSeries.vue
@@ -0,0 +1,21 @@
+<template>
+    <div class="col">
+        <div class="img-container">
+            <img :src="obj.thumb" :alt="obj.series">
+        </div>
+        <h4>{{obj.series}}</h4>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: 'CurrentSeries',
+        props: [
+            'obj'
+        ]
+    }
+</script>
+
+<style lang="scss" scoped>
+
+</style>
--- src/components/DarkContentSection.vue
+++ src/components/DarkContentSection.vue
@@ -1,24 +1,79 @@
 <template>
-    <section class="dark">
+    <section class="comics">
         <div class="wrapper">
-            Content goes here
+            <h2>Current Series</h2>
+            <div class="row">
+                <CurrentSeries v-for="(item,index) in comics" :key="index" :obj="item"/>
+            </div>
         </div>
+        <button class="load-more">Load More</button>
     </section>
 </template>
 
 <script>
+    import {dcComics} from '../data/dc-comics.js';
+    import CurrentSeries from './CurrentSeries.vue';
     export default {
         name: 'DarkContentSection',
+        components: {
+            CurrentSeries,
+        },
+        data(){
+            return {
+                comics: dcComics,
+            }
+        },
+        mounted(){
+            console.log(this.comics);
+        }
     }
 </script>
 
-<style lang="scss" scoped>
+<style lang="scss">
     @use '../assets/styles/partials/mixins' as *;
     @use '../assets/styles/partials/variables' as *;
-    .dark{
-        height: 130px;
+
+    .comics{
         background-color: $black;
         color: $white;
-        @include dflex;
+        position: relative;
+        display: flex;
+        flex-direction: column;
+        justify-content: center;
+        align-items: center;
+
+        h2{
+            background-color: $blue;
+            padding: 1rem 2rem;
+            position: absolute;
+            top: -2rem;
+        }
+        .row{
+            @include dflex;
+
+            .col{
+                align-self: flex-start;
+                width: calc(100% / 6);
+                padding: 2rem .75rem;
+                
+                .img-container{
+                    width: 185px;
+                    height: 185px;
+                    overflow: hidden;
+                }
+
+                h4{
+                    text-align: center;
+                    margin-top: 1rem;
+                }
+            }
+        }
+
+        .load-more{
+            background-color: $blue;
+            color: $white;
+            padding: .75rem 3.5rem;
+            margin-bottom: 1.3rem;
+        }
     }
 </style>
